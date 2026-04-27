43568a5a6487f5675f6c803cbabad49317f16b1e
judyraafat07 <judy.raafat07@eng-st.cu.edu.eg>
2026-04-28 00:03:30 +0300

    finished addflagAction some validations may be missing have to revisit

--- AddFlagAction.cpp
+++ AddFlagAction.cpp
@@ -47,10 +47,19 @@ void AddFlagAction::Execute()
 	// == Here are some guideline steps (numbered below) to implement this function ==
 
 	// 1-Create a flag object
+	Flag* pflag = new Flag(flagPos);
 	// 2-get a pointer to the Grid from the ApplicationManager
-	// 3-Add the flag object to the GameObject of its Cell:
-	// 4-Check if the flag was added and print an errror message if flag couldn't be added
+	Grid* pGrid = pManager->GetGrid();
 	
+	// 3-Add the flag object to the GameObject of its Cell:	
+	bool added = pGrid->AddObjectToCell(pflag);
+
+	// 4-Check if the flag was added and print an errror message if flag couldn't be added
+	if (!added)
+	{
+		// Print an appropriate message
+		pGrid->PrintErrorMessage("Error: Cell already has an object ! Click to continue ...");
+	}
 }
 
 AddFlagAction::~AddFlagAction()
