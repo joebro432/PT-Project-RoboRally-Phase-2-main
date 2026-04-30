4ab80a29f1b5ba06977d653852a08467fc6fb357
judyraafat07 <judy.raafat07@eng-st.cu.edu.eg>
2026-04-30 23:27:18 +0300

    Dangerzone object class Done

--- DangerZone.cpp
+++ DangerZone.cpp
@@ -1,6 +1,10 @@
 #include "DangerZone.h"
-
-
+#include <fstream>
+#include "Grid.h"
+#include "Output.h"
+#include "Input.h"
+#include "Player.h"
+#include "GameState.h"
 
 DangerZone::DangerZone(const CellPosition & dangerZonePosition): GameObject(dangerZonePosition)
 {
@@ -13,19 +17,44 @@ void DangerZone::Draw(Output * pOut) const
 
 void DangerZone::Apply(Grid* pGrid, GameState* pState, Player* pPlayer)
 {
-	///TODO: Implement this function as mentioned in the guideline steps (numbered below) below
+	///TODO:(DONE) Implement this function as mentioned in the guideline steps (numbered below) below
 
 
 	// == Here are some guideline steps (numbered below) to implement this function ==
 
 	// 1- Print a message "You have reached a danger zone. Click to continue ..." and wait mouse click
-
+	pGrid->GetOutput()->PrintMessage("You have reached a danger zone. Click to continue ...");
+	int x, y;
+	pGrid->GetInput()->GetPointClicked(x, y);
 	// 2- Apply the danger zone's effect by reducing the health of the player by 1 
-	// 3- Update the players info which is displayed (check Grid class and decide which function to use)
+	int health = pPlayer->GetHealth();
+	int newHealth = health - 1;
+	pPlayer->SetHealth(newHealth);//new health ll player 
+
+	//hn display message ll user 
+	pGrid->GetOutput()->PrintMessage("Your Health has been reduced by 1. Click to continue... ");
+	pGrid->GetInput()->GetPointClicked(x, y);
+
+	//check lw hwa kda dead
+	if (newHealth <= 0) {
+		pGrid->GetOutput()->PrintMessage("You have fallen into a danger zone and lost the game! Click to continue ...");
+		pGrid->GetInput()->GetPointClicked(x, y);
+		pState->SetEndGame(true);
+	}
 	
+	// 3- Update the players info which is displayed (check Grid class and decide which function to use)
+	pGrid->UpdateInterface(pState);
+	pGrid->GetOutput()->ClearStatusBar();
 }
 
 
 DangerZone::~DangerZone()
+{
+}
+void DangerZone::Save(ofstream& OutFile)
+{
+	OutFile << "DANGERZONE" << " " << position.GetCellNum() << endl;
+}
+void DangerZone::Load(ifstream& Infile)
 {
 }
--- DangerZone.h
+++ DangerZone.h
@@ -7,7 +7,8 @@ public:
 	virtual void Draw(Output* pOut) const; // Draws a danger zone in the cell
 
 	virtual void Apply(Grid* pGrid, GameState* pState, Player* pPlayer); // Applies the effect of the danger zone
-
+	virtual void Save(ofstream& OutFile);
+	virtual void Load(ifstream& Infile);
 
 	virtual ~DangerZone();
 };
