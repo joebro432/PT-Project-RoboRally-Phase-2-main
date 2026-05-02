1b10576a6988175f6e28bdf4370ef9352c437d63
judyraafat07 <judy.raafat07@eng-st.cu.edu.eg>
2026-05-03 00:33:08 +0300

    waterpit changed so it fits pdf not comments

--- WaterPit.cpp
+++ WaterPit.cpp
@@ -20,19 +20,20 @@ void WaterPit::Apply(Grid* pGrid, GameState* pState, Player* pPlayer)
 
 	// == Here are some guideline steps (numbered below) to implement this function ==
 
-	// 1- Print a message, make sure to edit this message according to which player wins "You drowned in a water pit. Player (0/1) wins !  Click to continue ..." and wait mouse click
-	if (pPlayer->getPlayerNum() == 0) {
-		pGrid->GetOutput()->PrintMessage("You drowned in a water pit. Player 1 wins !  Click to continue ...");
-	}
-	else {
-		pGrid->GetOutput()->PrintMessage("You drowned in a water pit. Player 0 wins !  Click to continue ...");
-	}
+	// a comment indicating that the player fell into water pit 
+	pGrid->GetOutput()->PrintMessage("You have fallen into a waterpit and have been sent to the starting cell. Click to cotinue...");
 	int x, y;
 	pGrid->GetInput()->GetPointClicked(x,y);
 
-	// 2- Apply the water pit's effect by setting the player's health which drowned to zero and ending the game
-	pPlayer->SetHealth(0);
-	pState->SetEndGame(true);
+	// 2- Apply the water pit's effect by reducing health by 3 w sending to start cell
+	int newhealth;
+	newhealth = pPlayer ->GetHealth() - 3;
+	pPlayer->SetHealth(newhealth); //kda 3mlna new health 
+
+	//nb3to b2a ll bdaya 
+	pGrid->UpdatePlayerCell(pPlayer, pGrid->GetStartCell()->GetCellPosition());
+
+
 	// 3- Update the players info which is displayed (check Grid class and decide which function to use)
 	pGrid->UpdateInterface(pState);
 
